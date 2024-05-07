d32c565da6f2b204af8d328f7fc49967f7f99ed9
nmarizani <n.marizani@alustudent.com>
2024-05-07 18:50:47 +0000

    My minimum operations files updated with test cases

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,9 +2,9 @@
 
 """ Module for 0-minoperations"""
 
-def minOperations(n):
-   
-#All outputs be at least 2 char: (min, Copy All => Paste)
+
+def minOperations(n):   
+    # All outputs be at least 2 char: (min, Copy All => Paste)
     if (n < 2):
 
         return 0
@@ -12,23 +12,23 @@ def minOperations(n):
     ops, root = 0, 2
     while root <= n: 
 
-#If n evenly divides by root
+        # If n evenly divides by root
 
         if n % root == 0:
 
-#Total even-divs by root = total ops
+            # Total even-divs by root = total ops
 
             ops += root
 
-#Set n to the remainder
+# Set n to the remainder
 
             n = n / root
 
-#Reduce root to find remaining smaller vals that evenly-divide n
+# Reduce root to find remaining smaller vals that evenly-divide n
 
             root -= 1
 
-#Increment root until it evenly-divides n
+# Increment root until it evenly-divides n
 
         root += 1
     return ops
